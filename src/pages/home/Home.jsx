4abaed0430bda5ebb5c10e988207bb4a7c5f453c
gitirom <wadiaromdane10@gmail.com>
2023-07-15 19:34:29 +0200

    I Complete the React chart compo..

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 
+import Chart from "../../components/chart/Chart";
 import Featuredinfo from "../../components/featuredinfo/Featuredinfo";
+import { userData } from "../../dummyData";
 import './home.css';
 
 export default function Home() {
     return (
         <div className='home' >
             <Featuredinfo />
+            <Chart data={userData} title="User Analytics " grid dataKey="Active User" />
         </div>
     )
     }
