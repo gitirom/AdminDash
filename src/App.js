c96a15336441e1b6426bc9321222ec7687ebb129
gitirom <wadiaromdane10@gmail.com>
2023-07-15 16:44:22 +0200

    I Complete the sidebar compo..

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import TopBar from "./components/topBar/TopBar";
+import Sidebar from "./components/sidebar/sidebar";
+import './app.css';
 
 
 function App() {
   return (
     <div >
       <TopBar />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
+      <div className="container" >
+        <Sidebar />
+        <div className="others">Other pages</div>
+      </div>
     </div>
   );
 }
