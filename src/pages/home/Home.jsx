628c312fd552caf0ad4c35132cae3d6ea4abb6ba
gitirom <wadiaromdane10@gmail.com>
2023-07-16 15:02:01 +0200

    I Complete the UserInfo compo..

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 
 import Chart from "../../components/chart/Chart";
 import Featuredinfo from "../../components/featuredinfo/Featuredinfo";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 import { userData } from "../../dummyData";
 import './home.css';
 
@@ -9,6 +11,10 @@ export default function Home() {
         <div className='home' >
             <Featuredinfo />
             <Chart data={userData} title="User Analytics " grid dataKey="Active User" />
+            <div className="homeWidget">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
     }
