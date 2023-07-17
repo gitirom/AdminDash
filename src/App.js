a8f5ce6e49f601aa04e9acb6d397f881d9738a47
gitirom <wadiaromdane10@gmail.com>
2023-07-17 17:37:11 +0200

    I Complete the userUpdate page and display users

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Sidebar from "./components/sidebar/sidebar";
 import './app.css';
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UserList from "./pages/home/userList/UserList";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route  path="/users" element={<UserList/>} />
+        <Route  path="/user/:userId" element={<User/>} />
         </Routes>
       </div>
     </Router>
